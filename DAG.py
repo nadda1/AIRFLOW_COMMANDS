0ca138b64bddfbfcb849595d29e0285a9ba3cf62
nadda1 <108094392+nadda1@users.noreply.github.com>
2024-08-07 23:01:07 +0300

    Update DAG.py

--- DAG.py
+++ DAG.py
@@ -8,3 +8,15 @@ default_arguments = {
 
 with DAG('example_etl', default_args=default_arguments) as etl_dag:
   pass
+# how to schedule a DAG 
+default_args = {
+  'owner': 'Engineering',
+  'start_date': datetime(2023, 11, 1),
+  'email': ['airflowresults@datacamp.com'],
+  'email_on_failure': False,
+  'email_on_retry': False,
+  'retries': 3,
+  'retry_delay': timedelta(minutes=20)
+}
+
+dag = DAG('update_dataflows', default_args=default_args, schedule_interval='30 12 * * 3')
